b30f56accbade771d387b1d4351ff90279e5df35
Prinik <Prinik@DESKTOP-37GDMCC>
2026-07-12 23:34:44 +0530

    Demo class implemented

--- src/com/java/core/priyanka/Demo.java
+++
@@ -1,8 +0,0 @@
-package com.java.core.priyanka;
-
-public class Demo {
-	public static void main(String[] args) {
-		System.out.println("My name is priyanka");
-	}
-
-}
---
+++ src/com/java/core/priyanka/Democlass.java
@@ -0,0 +1,7 @@
+package com.java.core.priyanka;
+
+public class Democlass {
+public static void main(String[] args) {
+	System.out.println("Priyanka");
+}
+}
