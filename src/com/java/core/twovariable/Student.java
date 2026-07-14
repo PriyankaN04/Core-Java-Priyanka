32954b461ad773d02ad4aea708d80f0213f94002
Prinik <Prinik@DESKTOP-37GDMCC>
2026-07-14 23:43:34 +0530

    Two class variable

---
+++ src/com/java/core/twovariable/Student.java
@@ -0,0 +1,24 @@
+package com.java.core.twovariable;
+
+public class Student {
+	
+	public String Fname;
+	
+	public String Lname;
+	
+	public long Phonenumber;
+	
+	public short age;
+	
+	public int YOB;
+	
+	public int ID;
+	
+	public double Percentage;
+	
+	public char Alphabet;
+	
+	
+	
+
+}
---
+++ src/com/java/core/twovariable/Test.java
@@ -0,0 +1,71 @@
+package com.java.core.twovariable;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Student S = new Student();
+
+		S.Fname = "Priyanka";
+		S.Lname = "Satav";
+		S.Phonenumber = 456789456;
+		S.age = 29;
+		S.YOB = 1997;
+		S.ID = 358501;
+		S.Percentage = 81.82;
+		S.Alphabet = 'P';
+
+		System.out.println(S.Fname);
+		System.out.println(S.Lname);
+		System.out.println(S.Phonenumber);
+		System.out.println(S.age);
+		System.out.println(S.YOB);
+		System.out.println(S.ID);
+		System.out.println(S.Percentage);
+		System.out.println(S.Alphabet);
+		
+		System.out.println("--------------------------------------------------------------");
+
+		Student S1 = new Student();
+		
+		S1.Fname = "Nikhil";
+		S1.Lname = "Satav";
+		S1.Phonenumber = 4758691;
+		S1.age = 30;
+		S1.YOB = 1997;
+		S1.ID = 35858;
+		S1.Percentage = 80.52;
+		S1.Alphabet = 'N';
+		
+		
+		System.out.println(S1.Fname);
+		System.out.println(S1.Lname);
+		System.out.println(S1.Phonenumber);
+		System.out.println(S1.age);
+		System.out.println(S1.YOB);
+		System.out.println(S1.ID);
+		System.out.println(S1.Percentage);
+		System.out.println(S1.Alphabet);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
