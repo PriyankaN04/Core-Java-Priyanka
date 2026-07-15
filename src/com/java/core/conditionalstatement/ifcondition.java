26d3216456e00d5b1736c8d339e2d4f58e97d30f
Prinik <Prinik@DESKTOP-37GDMCC>
2026-07-15 15:27:43 +0530

    If and nested condition

---
+++ src/com/java/core/conditionalstatement/ifcondition.java
@@ -0,0 +1,37 @@
+package com.java.core.conditionalstatement;
+
+public class ifcondition {
+
+	
+	public static void main(String[] args) {
+		
+	//value should be a ever or odd number - its a single condition
+		
+		int a = 21;
+		
+		if(a%2==0) {
+			System.out.println("It is a even number");
+			
+			
+		}
+		
+		else {
+			
+			System.out.println("It is a odd number");
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/com/java/core/conditionalstatement/nestedcondition.java
@@ -0,0 +1,52 @@
+package com.java.core.conditionalstatement;
+
+public class nestedcondition {
+
+	public static void main(String[] args) {
+	
+		
+		//condiiton - value should be divisible by 2 and 3 so it is a  good number - there can be multiple conditions
+		
+		int a = 30;
+		
+		
+		if(a%2 == 0) {
+			
+			System.out.println("Is divisible by 2");
+			
+			if (a%3 == 0) {
+				
+				System.out.println("Is divisible by 3");
+				System.out.println("Is good number");
+				
+			}
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
